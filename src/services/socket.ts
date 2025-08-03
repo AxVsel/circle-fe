a461ab6ef50c9030f8b4d6436db99f8d56d7c65d
Muh. Yaumil Aksah Hamid <assperacus77@gmail.com>
2025-08-03 15:23:13 +0800

    socet fix

--- src/services/socket.ts
+++ src/services/socket.ts
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 
 // GANTI localhost ke domain backend Railway
-const socket = io("https://circle-be-production-6eed.up.railway.app", {
+const socket = io("https://circle-be-production-6eed.up.railway.app/api/v1", {
   withCredentials: true,
 });
 
